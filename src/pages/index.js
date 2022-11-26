020833d940534f92ab224f94159876035f5ec480
migo1 <robertmigot08@gmail.com>
2022-11-26 08:10:16 +0300

    landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ export default function IndexPage() {
     <ThemeProvider theme={theme}>
       <Layout>
         <SEO
-          title="CRM Software Landing"
-          description="Collection of free top of the line startup landing templates built using react/ next js. Free to download, simply edit and deploy! Updated weekly!"
+          title="Ghala Kuu FMCG"
+          description="ghala kuu landing page"
         />
         <Banner />
         <Testimonials />
